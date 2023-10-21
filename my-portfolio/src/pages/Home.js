35bdeacba2bb66895391b738ded075fbb31759dd
Clement AeLBuS B <becker.clement@gmail.com>
2023-10-21 02:03:16 +0200

    Page Home + Background app

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -1,15 +1,28 @@
 import React from "react";
+import "../styles/Home.css";
+import PictureProfil from "../assets/images/photos/ProfilPicture.webp";
 
 const Home = () => {
   return (
     <div className="home-div">
-      <p>
-        Clément " Aelbus " B. <br />
-        32 ans.
-        <br />
-        Je suis Intégrateur Web, je fais aussi du gaming, de la musique, et du
-        graphisme.
-      </p>
+      <div className="home-info">
+        <h1>Porfolio</h1>
+        <h3>
+          Bienvenue <br />
+          dans mon univers. <br />
+          <br />
+          Bonne découverte !
+        </h3>
+        <p>N'hésitez pas à prendre contact avec moi.</p>
+      </div>
+      <div className="home-ael">
+        <div className="home-img">
+          <img className="home-pp" src={PictureProfil} alt="Photo AeLBuS" />
+        </div>
+        <div className="home-link">
+          <a>Télécharger CV</a>
+        </div>
+      </div>
     </div>
   );
 };
