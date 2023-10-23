1be58be5e26a42a1579ed49b0745a48c53492b03
Clement AeLBuS B <becker.clement@gmail.com>
2023-10-23 16:45:31 +0200

    Page Error

--- my-portfolio/src/pages/Error.js
+++ my-portfolio/src/pages/Error.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Error.css";
 
 const Error = () => {
   return (
     <div className="error-div">
       <div className="error-div-content">
-        <p className="error-number">error:404</p>
+        <p className="error-number">ERROR:404</p>
         <p className="error-text">
-        Désolé. À moins que vous ayez une machine à voyager dans le temps, ce contenu n'est pas disponible.
+          Désolé. À moins que vous ayez une machine à voyager dans le temps, ce
+          contenu n'est pas disponible.
         </p>
       </div>
       <Link to="/" className="error-link">
