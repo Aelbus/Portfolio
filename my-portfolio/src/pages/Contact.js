5ff4c09af0565e5ecc37f7e8e4dd15924c89750d
Clement AeLBuS B <becker.clement@gmail.com>
2023-10-27 20:52:10 +0200

    Changement Contact + responsive mobile

--- my-portfolio/src/pages/Contact.js
+++ my-portfolio/src/pages/Contact.js
@@ -1,45 +1,79 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "emailjs-com";
 import "../styles/Contact.css";
-import discord from "../assets/icons/Reseau/discord-logo.webp";
 
-const Contact = () => {
+function ContactForm() {
+  const [errors, setErrors] = useState({});
+  const inputRefs = {
+    name: useRef(null),
+    email: useRef(null),
+    subject: useRef(null),
+    message: useRef(null),
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setErrors({}); // Réinitialisez les erreurs avant chaque soumission
+
+    const requiredFields = ["name", "email", "subject", "message"];
+    let hasErrors = false;
+
+    requiredFields.forEach((field) => {
+      if (!e.target[field].value) {
+        hasErrors = true;
+        e.target[field].placeholder = "Champ requis";
+      }
+    });
+
+    if (hasErrors) {
+      return; // Ne soumettez pas le formulaire s'il y a des erreurs
+    }
+
+    emailjs
+      .sendForm(
+        "service_f9be7us",
+        "template_ojb2nka",
+        e.target,
+        "hFutUuL7N2e3bi_9a"
+      )
+      .then((result) => {
+        console.log("E-mail envoyé avec succès", result.text);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de l'envoi de l'e-mail", error);
+      });
+  };
+
   return (
     <div className="contact-div">
-      <p>Me contacter</p>
       <div className="contact-container">
-        <div className="mail">
-          Email
-          <div class="letter-image">
-            <div class="animated-mail">
-              <div class="back-fold"></div>
-              <div class="letter">
-                <a class="contactlink" href="mailto:becker.clement@gmail.com">
-                  Contactez moi !
-                </a>
-                <div class="letter-border"></div>
-                <div class="letter-title"></div>
-                <div class="letter-context"></div>
-                <div class="letter-stamp">
-                  <div class="letter-stamp-inner"></div>
-                </div>
-              </div>
-              <div class="top-fold"></div>
-              <div class="body"></div>
-              <div class="left-fold"></div>
-            </div>
-            <div class="shadow"></div>
+        <h2>Contactez-moi</h2>
+        <form className="contact-form" onSubmit={sendEmail}>
+          <div className="contact-label">
+            <label>Nom:</label>
+            <input type="text" name="name" ref={inputRefs.name} />
           </div>
-        </div>
-        <div className="discord">
-          Discord <br />
-          aelbus
-          <a href="https://discord.gg/PvyFbgvuvk">
-            <img className="discord-logo" src={discord} alt="Discord" />
-          </a>
-        </div>
+          <div className="contact-label">
+            <label>Email:</label>
+            <input type="email" name="email" ref={inputRefs.email} />
+          </div>
+          <div className="contact-label">
+            <label>Sujet:</label>
+            <input type="text" name="subject" ref={inputRefs.subject} />
+          </div>
+          <div className="contact-label">
+            <label>Téléphone:</label>
+            <input type="tel" name="phone" />
+          </div>
+          <div className="contact-label">
+            <label>Message:</label>
+            <textarea name="message" ref={inputRefs.message} />
+          </div>
+          <button type="submit">Envoyer</button>
+        </form>
       </div>
     </div>
   );
-};
+}
 
-export default Contact;
+export default ContactForm;
